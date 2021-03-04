eec224e6ceb517331485de227e54ef979e2a2876
jrslagle <jrslagle@gmail.com>
2021-03-03 23:36:42 -0700

    just PEP 8 formatting

--- run.py
+++ run.py
@@ -75,6 +75,7 @@ def display_page(pathname):
     else:
         return dcc.Markdown('## Page not found')
 
+
 # Run app server: https://dash.plot.ly/getting-started
 if __name__ == '__main__':
     app.run_server(debug=True)
